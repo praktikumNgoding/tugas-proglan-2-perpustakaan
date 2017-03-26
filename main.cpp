d3d96fabc96127b90cafedd859beddaeb1c7af55
dnuridhuha <dwiki.nuridhuha@outlook.com>
2017-03-26 16:00:46 +0700

    Update main.cpp
    ini cabang dari saya <Dwiki>

--- main.cpp
+++ main.cpp
@@ -2,6 +2,49 @@
 
 using namespace std;
 
+class buku {
+	public:
+		string judul, penulis, deskripsi, penerbit, kota, kategori;
+		int hal = 0, tahun = 0;
+
+		buku() {}
+
+		void isi_buku(string kategori, string judul, string penulis, string deskripsi, string penerbit, string kota, int hal, int tahun){
+			this->hal = hal;
+			this->penulis = penulis;
+			this->penerbit = penerbit;
+			this->deskripsi = deskripsi;
+			this->judul = judul;
+			this->tahun = tahun;
+			this->kota = kota;
+			this->kategori = kategori;
+		}
+};
+
+class perpustakaan : public buku {
+	private :
+		void memasukan_data(string kategori, string judul, string penulis, string deskripsi, string penerbit, string kota, int hal, int tahun,perpustakaan *sip ){ // bantuan Nomer 2
+			for(int i = 0; ;i++) {
+				if(sip[i].buku::tahun == 0) {
+					sip[i].buku::judul = judul;
+					sip[i].buku::deskripsi = deskripsi;
+					sip[i].buku::kota = kota;
+					sip[i].buku::penerbit = penerbit;
+					sip[i].buku::tahun = tahun;
+					sip[i].buku::hal = hal;
+					sip[i].buku::kategori = kategori;
+					sip[i].buku::penulis = penulis;
+
+					break;
+				}
+			}
+		}
+
+	public:
+		perpustakaan(){}
+
+};
+
 int main() {
 	cout << "Testing" << endl;
 return 0;
